ccadd702d4c3310e0c2595252d8aeac103aa8628
mwtjc <mwtjc@outlook.com>
2025-03-19 15:30:00 +0800

    621不做，先实现623与624 #11

--- PySideApp/Libs/TestModuleLib/GAT1411_3_4_2017.py
+++ PySideApp/Libs/TestModuleLib/GAT1411_3_4_2017.py
@@ -1,30 +1,55 @@
 import pandas as pd
 from PySideApp.Libs.test_tasks_lib import TestModule, TestParamInput, TestStep
 
-TYPE = 'GA/T 1411.3/4-2017 警用无人驾驶航空器系统 第三部分、第四部分'
+TYPE = 'GA/T 1411.3/4-2017 警用无人驾驶航空器系统 第三、四部分'
 
-class T6_2_1(TestModule):
+class T1411_623_624(TestModule):
     def __init__(self):
         super().__init__(
             test_type=TYPE,
-            name='6.2.1 有效载荷系数检验',
-            search_keywords=['1411', '载荷', '624'],
+            name='6.2.3实用升限/6.2.4实际使用高度检测',
+            search_keywords=['623', "624", "升限", "高度" ],
         )
+        # 轮数指示
+        self.round_count = 5
+
+    def init_test_task(self, input_param_list, uuid: int, note: str | None = None):
+        # 先取得输入参数的值
+        for param in input_param_list:
+            if param.name == '普通参数':
+                self.param1 = float(param.value)  # 取某变量到步骤文本中
+                break
+
+        super().init_test_task(uuid, input_param_list, note)
 
     def get_input_list(self):
         return [
-            TestParamInput('半径 大于', 20.0, 'm')
+            TestParamInput('xy点 经 纬', 0.0, data_type="xy_point"),
+            TestParamInput('xyz点 经 纬', 0.0, data_type="xyz_point"),
+            TestParamInput('z高度', 0.0, data_type="z"),
+            TestParamInput('整数参数', 1, data_type="int", unit="次"),
+            TestParamInput('普通参数', 20.0, unit="米"),
+            TestParamInput('轮数', 5, data_type="int", unit="轮"),
         ]
 
     def get_step_list(self):
-        return [
-            TestStep('无人机起飞，飞行到大约10米高度'),
-            TestStep('第一轮：飞行到起点悬停，记为A1点'),
-            TestStep('以5m/s的速度操作无人机向设定的飞行半径飞行，确认飞机已经到达指定的飞行半径，记为B1点', True),
-            TestStep('记录完成。'),
+        list_temp = [
+            TestStep('测试开始：纯文本步骤'),
+            TestStep(f'取参数步骤：{self.param1}'),
         ]
+        for i in range(self.round_count):
+            round_pre = f"第{i + 1}/{self.round_count}轮："
+            list_temp.extend([
+                TestStep(f'{round_pre}步骤'),
+                TestStep(f'{round_pre}需要取值的步骤', True),
+            ])
+        list_temp.extend([
+            TestStep('记录完成。'),
+        ])
 
-    def _perform_calculation(self, data:pd.DataFrame):
+        return list_temp
+
+    def _perform_calculation(self, data: pd.DataFrame):
         inputs = []
         for param in self.test_task.input_param_list:
             inputs.append(param.value)
@@ -39,3 +64,4 @@ class T6_2_1(TestModule):
                     """
         self.test_task.org_dataframe = data
         return '1212.4', '0.05'
+
